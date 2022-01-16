ff7c84de8ec62176e2733ad9dbb7567eca9c8f8b
lukechaplin <lukechaplin@gmail.com>
2022-01-16 19:21:28 +0000

    addedmobileoptimization

--- main.js
+++ main.js
@@ -8,12 +8,4 @@ function turnOnDarkMode() {
 document.body.classList.toggle("dark-mode-theme")
 }
   
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function addRemoveRepsonsive() {
-    var topNav = document.getElementById("myTopnav");
-    if (topNav.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      topNav.className = "topnav";
-    }
-  }
+
